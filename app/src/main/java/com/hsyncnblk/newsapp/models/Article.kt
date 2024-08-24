c378fcf03d6389a0a758c3055c21a13fe471e1b4
huseyin cinibulak <huseyincnblk@gmail.com>
2024-08-24 23:51:42 +0300

    retrofit

---
+++ app/src/main/java/com/hsyncnblk/newsapp/models/Article.kt
@@ -0,0 +1,23 @@
+package com.hsyncnblk.newsapp.models
+
+
+import com.google.gson.annotations.SerializedName
+
+data class Article(
+    @SerializedName("author")
+    var author: String?,
+    @SerializedName("content")
+    var content: Any?,
+    @SerializedName("description")
+    var description: Any?,
+    @SerializedName("publishedAt")
+    var publishedAt: String?,
+    @SerializedName("source")
+    var source: Source?,
+    @SerializedName("title")
+    var title: String?,
+    @SerializedName("url")
+    var url: String?,
+    @SerializedName("urlToImage")
+    var urlToImage: Any?
+)
---
+++ app/src/main/java/com/hsyncnblk/newsapp/models/NewsResponse.kt
@@ -0,0 +1,13 @@
+package com.hsyncnblk.newsapp.models
+
+
+import com.google.gson.annotations.SerializedName
+
+data class NewsResponse(
+    @SerializedName("articles")
+    var articles: MutableList<Article?>?,
+    @SerializedName("status")
+    var status: String?,
+    @SerializedName("totalResults")
+    var totalResults: Int?
+)
