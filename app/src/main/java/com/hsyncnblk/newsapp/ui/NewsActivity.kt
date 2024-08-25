03c8e97c7197fe83bdcdf9fd2451864cbebeba28
huseyin cinibulak <huseyincnblk@gmail.com>
2024-08-25 14:46:43 +0300

    viewModel and Factory

--- app/src/main/java/com/hsyncnblk/newsapp/ui/NewsActivity.kt
+++ app/src/main/java/com/hsyncnblk/newsapp/ui/NewsActivity.kt
@@ -2,11 +2,29 @@ package com.hsyncnblk.newsapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.hsyncnblk.newsapp.R
+import com.hsyncnblk.newsapp.databinding.ActivityNewsBinding
+import com.hsyncnblk.newsapp.db.ArticleDatabase
+import com.hsyncnblk.newsapp.repository.NewsRepository
 
 class NewsActivity : AppCompatActivity() {
+
+    lateinit var newsViewModel: NewsViewModel
+    lateinit var binding: ActivityNewsBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_news)
+        binding = ActivityNewsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val newsRepository = NewsRepository(ArticleDatabase(this))
+        val viewModelProviderFactory = NewsViewModelProviderFactory(application, newsRepository)
+        newsViewModel = ViewModelProvider(this, viewModelProviderFactory).get(NewsViewModel::class.java)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.newsNavHostFragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        binding.bottomNavigationView.setupWithNavController(navController)
     }
 }
